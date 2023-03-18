dfb6acac529439df27ba049a3018659d7ec1026b
dayvd <allysondayvd@gmail.com>
2023-03-18 19:07:50 -0300

    os 4 tipos de métodos HTTP

--- HTTP.js
+++ HTTP.js
@@ -1,28 +1,39 @@
 if(self.fetch){
-    // console.log("tem")
     (async () => {
-        // const json = await obterPost(2);
-        const json = await incluirPost({
-            userId: 2,
-            title: "Estou criando um novo post",
-            body: "Estou criando um novo corpo"
-        })
-        console.log(json);
+        // const json = await incluirPost({
+        //     userId: 2,
+        //     title: "Estou criando um novo post",
+        //     body: "Estou criando um novo corpo"
+        // })
+
+
+        // const json = await upDatePost(
+        //     {
+        //         title: "Apenas o título está sendo atualizado"
+        //     }, 1)
+        // console.log(json);
+
+        const json = await await deletePost(1);
+        console.log(json)
     })();
 }else{
-    console.log("não")
+    console.log("não tem")
+}
+
+async function getJsonResponse(response){
+    if(!response.ok){
+        throw new Error(response.status + " " + response.statusText)
+    }
+    const json = await response.json();
+    return json;
 }
 
 
 // o método abaixo seria uma requisição do tipo GET, não exigiria um segundo parâmetro de configuração no método fetch
 async function obterPost(id){
     const response = await fetch("https://jsonplaceholder.typicode.com/posts/"+id);
-    if(!response.ok){
-        throw new Error(response.status + " " + response.statusText)
-    }
-
-    const json = await response.json();
-    return json;
+    return getJsonResponse(response);
+    
 }
 
 
@@ -33,12 +44,30 @@ async function incluirPost(data){
         headers: {
             'content-type': 'application/json;charset=UTF-8'
         }
-    });
-
-    if(!response.ok){
-        throw new Error(response.status + " " + response.statusText)
     }
+    )
+    return getJsonResponse(response);
+}
 
-    const json = await response.json();
-    return json;
+async function upDatePost(data, id){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/"+id,{
+        method: 'PATCH',
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json;charset=UTF-8'
+        }
+    }
+    )
+    return getJsonResponse(response);
+}
+
+async function deletePost(id){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/"+id,{
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json;charset=UTF-8'
+        }
+    }
+    )
+    return getJsonResponse(response);
 }
